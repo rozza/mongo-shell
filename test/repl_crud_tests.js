c3ce9fe51175c5101219a34fde0070dfef1987ee
Christian Kvalheim <christkv@gmail.com>
2017-01-19 18:43:44 +0100

    refactored to use async functionality in node and added updateOne/updateMany to collection

--- test/repl_crud_tests.js
+++ test/repl_crud_tests.js
@@ -91,5 +91,44 @@ describe('Repl CRUD tests', () => {
         console.log(err);
       });
     });
+
+    it('should correctly upsert a single document using updateOne', (done) => {
+      co(function*() {
+        // Init context
+        const initContext = Object.assign({}, global, {});
+        // Create a context for execution
+        var context = Object.assign(vm.createContext(initContext), {
+          db: Db.proxy(client.s.databaseName, client),
+          require: require,
+        });
+
+        // Create a repl instance
+        const repl = new REPL(client, context, {
+        });
+        // Start the repl
+        const _repl = repl.start();
+        // Execute command
+        _repl.eval('db.tests2.updateOne({f1:1}, {f1:1}, { upsert:true })', context, '', function(err, result) {
+          console.log("======================================== 0")
+          console.dir(err)
+          console.dir(result)
+          // assert.equal(null, err);
+          // assert.equal(1, result.insertedIds.length);
+
+          // Render the repl final text
+          let string = _repl.writer(result);
+          string = string.replace(/\n|\\n/g, '');
+          console.log("======================================== 0")
+          console.log(string)
+
+          // assert.equal(
+          //   `{  "acknowledged": true,  "insertedId": ObjectId("${result.insertedIds[0].toString()}")}`.trim(),
+          //   string.trim());
+          done();
+        });
+      }).catch(function(err) {
+        console.log(err);
+      });
+    });
   });
 });
